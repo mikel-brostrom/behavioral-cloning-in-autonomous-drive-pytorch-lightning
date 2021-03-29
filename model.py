083313bbc3042de0bf46536bfd6f037788aa2aa6
mikel.brostrom <mikel.brostrom@cevt.se>
2021-03-29 13:38:04 +0200

    file previously called light_train and light_module

--- model.py
+++ model.py
@@ -1,165 +1,50 @@
+import pytorch_lightning as pl
+import pickle
 import torch
-import torch.nn as nn
-import torch.nn.init as init
-from torch.autograd import Variable
-import torch.optim as optim
-from torch.utils.data import DataLoader
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
-from torchvision.utils import save_image
-#from torchnet.meter import AverageValueMeter
-import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+import numpy as np
+
+from model_architectures.registry import create_model
 
 
-class CarModel(nn.Module):
-    def __init__(self):
-        super(CarModel, self).__init__()
-        self.conv_layers = nn.Sequential(
-            # input is batch_size x 3 x 66 x 200
-            nn.Conv2d(3, 24, 5, stride=2, bias=False),
-            #nn.ELU(0.2, inplace=True),
-            nn.ELU(),
-            nn.Conv2d(24, 36, 5, stride=2, bias=False),
-            nn.ELU(),
-            nn.BatchNorm2d(36),
-            
-            nn.Conv2d(36, 48, 5, stride=2, bias=False),
-            nn.ELU(),
-            nn.BatchNorm2d(48),
-            
-            nn.Conv2d(48, 64, 3, stride=1, bias=False),
-            nn.ELU(),
-            nn.BatchNorm2d(64),
-            
-            nn.Conv2d(64, 64, 3, stride=1, bias=False),
-            nn.ELU(),
-            nn.Dropout(p=0.4)
-        )
-        self.linear_layers = nn.Sequential(
-            #input from sequential conv layers
-            nn.Linear(in_features=64*1*18, out_features=100, bias=False),
-            nn.ELU(),
-            nn.Linear(in_features=100, out_features=50, bias=False),
-            nn.ELU(),
-            nn.Linear(in_features=50, out_features=10, bias=False),
-            nn.ELU(),
-            nn.Linear(in_features=10, out_features=1, bias=False))
-        self._initialize_weights()
-        
-    # custom weight initialization
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                init.normal(m.weight, mean=0, std=0.02)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.normal(m.weight, mean=1, std=0.02)
-                init.constant(m.bias, 0)
+class LightningModel(pl.LightningModule):
+    def __init__(self, config):
+        super().__init__()
+        self.save_hyperparameters()
+        self.model = create_model('car_simple_model')
 
-    def forward(self, input):
-        output = self.conv_layers(input)
-        output = output.view(output.size(0), 64*1*18)
-        output = self.linear_layers(output)
-        return output
-    
-class CarDenseModel(nn.Module):
-    def __init__(self):
-        super(CarDenseModel, self).__init__()
-        self.conv_layers = nn.Sequential(
-            # input is batch_size x 3 x 66 x 200
-            nn.Conv2d(3, 24, 5, stride=2, bias=False),
-            #nn.ELU(0.2, inplace=True),
-            nn.ELU(),
-            nn.Conv2d(24, 36, 5, stride=2, bias=False),
-            nn.ELU(),
-            nn.BatchNorm2d(36),
-            
-            nn.Conv2d(36, 48, 5, stride=2, bias=False),
-            nn.ELU(),
-            nn.BatchNorm2d(48),
-            
-            nn.Conv2d(48, 64, 3, stride=1, bias=False),
-            nn.ELU(),
-            nn.BatchNorm2d(64),
-            
-            nn.Conv2d(64, 64, 3, stride=1, bias=False),
-            nn.ELU(),
-            nn.Dropout(p=0.4)
-        )
-        self.linear_layers = nn.Sequential(
-            #input from sequential conv layers
-            nn.Linear(in_features=64*1*18+9*32*99, out_features=100, bias=False),
-            nn.ELU(),
-            nn.Linear(in_features=100, out_features=50, bias=False),
-            nn.ELU(),
-            nn.Linear(in_features=50, out_features=10, bias=False),
-            nn.ELU(),
-            nn.Linear(in_features=10, out_features=1, bias=False))
-        
-        self.shortcut = nn.Sequential(
-                nn.Conv2d(3, 9, 3, stride=2, bias=False),
-                nn.BatchNorm2d(9)
-            )
-        
-        self._initialize_weights()
-        
-    # custom weight initialization
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                init.normal(m.weight, mean=0, std=0.02)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.normal(m.weight, mean=1, std=0.02)
-                init.constant(m.bias, 0)
+    def training_step(self, batch, _):
 
-    def forward(self, input):
-        output = self.conv_layers(input)
-        
-        output = output.view(output.size(0), 64*1*18)
-        input_shortcut = self.shortcut(input)
-        
-        input_shortcut = input_shortcut.view(input_shortcut.size(0), 9*32*99)
-        output_sum = torch.cat((output, input_shortcut), 1).view(output.size(0), 64*1*18+9*32*99)
-        output_sum = self.linear_layers(output_sum)
-        return output_sum
-    
-    
-class CarSimpleModel(nn.Module):
-    def __init__(self):
-        super(CarSimpleModel, self).__init__()
-        self.conv_layers = nn.Sequential(
-            # input is batch_size x 3 x 16 x 32
-            nn.Conv2d(3, 24, 3, stride=2, bias=False),
-            nn.ELU(),
-            nn.Conv2d(24, 48, 3, stride=2, bias=False),
-            nn.MaxPool2d(4, stride=4),
-            nn.Dropout(p=0.25)
-        )
-        self.linear_layers = nn.Sequential(
-            #input from sequential conv layers
-            nn.Linear(in_features=48*4*19, out_features=50, bias=False),
-            nn.ELU(),
-            nn.Linear(in_features=50, out_features=10, bias=False),
-            nn.Linear(in_features=10, out_features=1, bias=False),
-        )
-        self._initialize_weights()
-        
-    # custom weight initialization
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                init.normal(m.weight, mean=0, std=0.02)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.normal(m.weight, mean=1, std=0.02)
-                init.constant(m.bias, 0)
+        x, y = batch
+        x, y = x.float(), y.float()
+        x = torch.reshape(x, (x.shape[0], x.shape[-1], x.shape[1], x.shape[2]))
 
-    def forward(self, input):
-        
-        input = input.view(input.size(0), 3, 75, 320)
-        output = self.conv_layers(input)
-        #print('size ' + str(output.size()))
-        output = output.view(output.size(0), -1)
-        output = self.linear_layers(output)
-        return output    
+        y_hat = self.model(x)
+        loss = F.mse_loss(y_hat.squeeze(), y)
+
+        metrics = {'train/loss': loss, 'step': self.current_epoch}
+        self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+
+    def validation_step(self, batch, _):
+
+        x, y = batch
+        x, y = x.float(), y.float()
+        x = torch.reshape(x, (x.shape[0], x.shape[-1], x.shape[1], x.shape[2]))
+
+        y_hat = self.model(x)
+        loss = F.mse_loss(y_hat.squeeze(), y)
+
+        metrics = {'valid/loss': loss, 'step': self.current_epoch}
+        self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+
+    def configure_optimizers(self):
+        """Initializes a optimizer to use while traning.
+
+        Returns:
+            torch.optim.Optimizer: Traning optimizer.
+        """
+        return torch.optim.Adam(self.model.parameters(), lr=0.02)
--- train.py
+++ train.py
@@ -1,144 +1,49 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from utils import *
 import argparse
-import os
 
-from model import *
-
-parser = {
-    'data_dir': './data/',
-    'nb_epoch': 50,
-    'test_size': 0.1,
-    'learning_rate': 0.0001,
-    'samples_per_epoch': 64,
-    'batch_size': 36,
-    'cuda': True,
-    'seed': 7
-}
-args = argparse.Namespace(**parser)
-args.cuda = args.cuda and torch.cuda.is_available()
-
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
-
-def load_data(args):
-    """
-    Load training data and split it into training and validation set
-    """
-    #reads CSV file into a single dataframe variable
-    data_df = pd.read_csv(os.path.join(os.getcwd(), args.data_dir, 'driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
-
-    #yay dataframes, we can select rows and columns by their names
-    #we'll store the camera images as our input data
-    X = data_df[['center', 'left', 'right']].values
-    #and our steering commands as our output data
-    y = data_df['steering'].values
-
-    #now we can split the data into a training (80), testing(20), and validation set
-    #thanks scikit learn
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=args.test_size, random_state=0, shuffle=True)
-
-    return X_train, X_valid, y_train, y_valid
-
-X_train, X_valid, y_train, y_valid = load_data(args)
-
-transformations = transforms.Compose([transforms.Lambda(lambda x: x/127.5 - 1)                                    
-                                     ])
-print('\n\n', args.data_dir)
-#train_set = CarDataset(X_train, y_train, args.data_dir, False,transformations)
-train_set = CarDataset3Img(X_train, y_train, args.data_dir,transformations)
-valid_set = CarDataset(X_valid, y_valid, args.data_dir, False, transformations)
-
-train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=4)
-valid_loader = DataLoader(valid_set, batch_size=args.batch_size, shuffle=True, num_workers=4)
-
-def toVariable(data, use_cuda):
-    input, target = data
-    input, target = Variable(input.float()), Variable(target.float())
-    if use_cuda:
-        input, target = input.cuda(), target.cuda()
-    
-    return input, target
-
-
-
-
-# Training
-def train(epoch, net, dataloader, optimizer, criterion, use_cuda):
-    net.train()
-    train_loss = 0
-    
-    for batch_idx, (centers, lefts, rights) in enumerate(dataloader):
-
-        optimizer.zero_grad()
-        centers, lefts, rights = toVariable(centers, use_cuda), \
-                                 toVariable(lefts, use_cuda), \
-                                 toVariable(rights, use_cuda)
-        datas = [lefts, rights, centers]        
-        for data in datas:
-            imgs, targets = data
-            outputs = net(imgs)
-            loss = criterion(outputs.squeeze(), targets)
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss.item()
-            
-        if batch_idx % 100 == 0:
-            print('Loss: %.3f '
-                % (train_loss/((batch_idx+1)*3)))
-
-def valid(epoch, net, validloader, criterion, use_cuda):
-    global best_loss
-    net.eval()
-    valid_loss = 0
-    for batch_idx, (inputs, targets) in enumerate(validloader):
-        inputs, targets = Variable(inputs.float()), Variable(targets.float())
-        if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda()
-        outputs = net(inputs)
-        loss = criterion(outputs.squeeze(), targets)
-
-        valid_loss += loss.item()
-        
-        avg_valid_loss = valid_loss/(batch_idx+1)
-        if batch_idx % 100 == 0:
-            print('Valid Loss: %.3f '
-                % (valid_loss/(batch_idx+1)))
-        if avg_valid_loss <= best_loss:
-            best_loss = avg_valid_loss
-            print('Best epoch: ' + str(epoch))
-            state = {
-                'net': net.module if args.cuda else net,
-            }
-            torch.save(state, './models/current.h5')
-
+from datamodule import DataModule
 from model_architectures.registry import create_model
-
-net = create_model('car_simple_model')
-optimizer = optim.Adam(net.parameters(), lr=args.learning_rate)
-
-if args.cuda:
-    net.cuda()
-    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
-    cudnn.benchmark = True
-
-criterion = nn.MSELoss()
-best_loss = 0.999
+from pytorch_lightning.callbacks import ModelCheckpoint
+import pytorch_lightning as pl
+from model import LightningModel
 
 
+def train():
+    """Train ML model.
 
-for epoch in range(0,15):
-    #optimizer = lr_scheduler(optimizer, epoch, lr_decay_epoch=args.lr_decay_epoch)	
-    print('\nEpoch: %d' % epoch)
-    train(epoch, net, train_loader, optimizer, criterion, args.cuda)
-    valid(epoch, net, valid_loader, criterion, args.cuda)
+    Args:
+        config (dict): Configuration.
+        experiment (str): Experiment name
+        data_dir (str): Folder where to load split from
+    """
+    model = LightningModel('car_simple_model')
+    dm = DataModule()
+    dm.prepare_data()
+    dm.setup()
+    logger = pl.loggers.TensorBoardLogger(
+        save_dir=str('./logs'),
+        name='car_simple_model',
+        default_hp_metric=False,
+    )
 
-state = {
-        'net': net.module if args.cuda else net,
-        }
+    checkpoint_callbacks = [
+        ModelCheckpoint(monitor="valid/loss", mode="min", filename="best", save_last=True),
+    ]
+    trainer = pl.Trainer(
+        gpus=-1, max_epochs=40, logger=logger, callbacks=checkpoint_callbacks, enable_pl_optimizer=False
+    )
+    trainer.fit(model, dm)
 
-torch.save(state, './models/last.h5')
+    logger.finalize("Training success")
+    logger.save()
+
+
+def main(args):
+    train()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Train RUL model", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    args = parser.parse_args()
+    main(args)
