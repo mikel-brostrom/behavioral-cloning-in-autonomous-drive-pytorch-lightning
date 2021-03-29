f3adf69f72b40b64d8fdc0dbf647605e159273b8
mikel.brostrom <mikel.brostrom@cevt.se>
2021-03-29 13:39:27 +0200

    cv2 augmentations now albumentations

--- dataset.py
+++ dataset.py
@@ -9,6 +9,10 @@ import pandas as pd
 from torch.utils.data import DataLoader, Dataset
 from turbojpeg import TurboJPEG
 import torchvision.transforms as transforms
+import albumentations as albu
+from PIL import Image
+from time import sleep
+Image.MAX_IMAGE_PIXELS = 2
 
 
 IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 16, 32, 1
@@ -20,14 +24,14 @@ class CarDataset(Dataset):
     Dataset wrapping images and target steering_angle.
     """
 
-    def __init__(self, X, y, is_training, transform=None):
+    def __init__(self, X, y, is_training, transform=None, debug=False):
         self.X = X
         self.y = y
         self.is_training = is_training
         self.transform = transform
         self.jpg_reader = TurboJPEG()
+        self.debug = debug
 
-        self.transformations = transforms.Compose([transforms.Lambda(lambda x: x/127.5 - 1)])
 
     def __getitem__(self, index):
         image_path = self.X[index]
@@ -39,67 +43,43 @@ class CarDataset(Dataset):
                     jpgfile.read()
                 )
 
-        image = preprocess(image)
-        image, steering_angle = augument_img(image, steering_angle)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        image, steering_angle = random_flip(image, steering_angle)
 
-        #print('\nBefore preprocess', image.shape)
-        
-        image = self.transformations(image)
-        #image = image.reshape(image.shape[0], image.shape[-1], image.shape[1], image.shape[2])
+        if self.is_training is True:
+            self.image_transforms = albu.Compose(
+                [
+                    # (160, 320, 3) ---> (60-135, 320, 3), remove sky and car front
+                    albu.Crop(0, 60, 320, 160-25),
+                    albu.Blur(blur_limit=3, p=0.1),
+                    albu.RGBShift(p=0.1),
+                    albu.HueSaturationValue(p=0.1),
+                    albu.RandomBrightnessContrast(p=0.1),
+                    albu.RandomGamma(p=0.1),
+                    albu.RandomShadow(p=0.1),
+                    albu.Normalize(always_apply=True)
+                ]
+            )
+        else:
+            self.image_transforms = albu.Compose(
+                [
+                    albu.Crop(0, 60, 320, 160-25),
+                    albu.Normalize(always_apply=True)
+                ]
+            )
 
-        #print('\nAfter preprocess', image[0].shape)
-        #print(type(steering_angle))
+        image = self.image_transforms(image=image)["image"]
+
+        # visualize augmentations
+        if self.debug is True:
+            img = Image.fromarray(image, 'RGB')
+            img.show()
+            sleep(3)
 
         return image, steering_angle
     
     def __len__(self):
         return self.X.shape[0]
-
-def augument_img(image, steering_angle):
-    if np.random.rand() < 0.6:
-        image, steering_angle = random_flip(image, steering_angle)
-        image, steering_angle = random_translate(image, steering_angle, 100, 10)
-        image = random_shadow(image)
-        image = random_brightness(image)
-    return image, steering_angle
-
-def crop(image):
-    """
-    Crop the image (removing the sky at the top and the car front at the bottom)
-    """
-    return image[60:-25, :, :] # remove the sky and the car front
-
-def rgb2hsv(image):
-    """
-    Convert the image from RGB to HSV 
-    """
-    return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-
-def preprocess(image):
-    """
-    Combine all preprocess functions into one
-    """
-    image = crop(image)
-    image = rgb2hsv(image)
-    #image = self.resize(image)
-
-    return image
-
-
-def resize(image):
-    """
-    Resize the image to the input shape used by the network model
-    """
-    return cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_AREA)
-
-
-def rgb2yuv(image):
-    """
-    Convert the image from RGB to YUV (This is what the NVIDIA model does)
-    """
-    return cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-
-
 
 def random_flip(image, steering_angle):
     """
@@ -109,52 +89,3 @@ def random_flip(image, steering_angle):
         image = cv2.flip(image, 1)
         steering_angle = -steering_angle
     return image, steering_angle
-
-def random_translate(image, steering_angle, range_x, range_y):
-    """
-    Randomly shift the image virtially and horizontally (translation).
-    """
-    trans_x = range_x * (np.random.rand() - 0.5)
-    trans_y = range_y * (np.random.rand() - 0.5)
-    steering_angle += trans_x * 0.002
-    trans_m = np.float32([[1, 0, trans_x], [0, 1, trans_y]])
-    height, width = image.shape[:2]
-    image = cv2.warpAffine(image, trans_m, (width, height))
-    return image, steering_angle
-
-def random_shadow(image):
-    """
-    Generates and adds random shadow
-    """
-    # (x1, y1) and (x2, y2) forms a line
-    # xm, ym gives all the locations of the image
-    x1, y1 = IMAGE_WIDTH * np.random.rand(), 0
-    x2, y2 = IMAGE_WIDTH * np.random.rand(), IMAGE_HEIGHT
-    xm, ym = np.mgrid[0:IMAGE_HEIGHT, 0:IMAGE_WIDTH]
-
-    # mathematically speaking, we want to set 1 below the line and zero otherwise
-    # Our coordinate is up side down.  So, the above the line: 
-    # (ym-y1)/(xm-x1) > (y2-y1)/(x2-x1)
-    # as x2 == x1 causes zero-division problem, we'll write it in the below form:
-    # (ym-y1)*(x2-x1) - (y2-y1)*(xm-x1) > 0
-    mask = np.zeros_like(image[:, :, 1])
-    mask[np.where((ym - y1) * (x2 - x1) - (y2 - y1) * (xm - x1) > 0)] = 1
-
-    # choose which side should have shadow and adjust saturation
-    cond = mask == np.random.randint(2)
-    s_ratio = np.random.uniform(low=0.2, high=0.5)
-
-    # adjust Saturation in HLS(Hue, Light, Saturation)
-    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-    hls[:, :, 1][cond] = hls[:, :, 1][cond] * s_ratio
-    return cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
-
-def random_brightness(image):
-    """
-    Randomly adjust brightness of the image.
-    """
-    # HSV (Hue, Saturation, Value) is also called HSB ('B' for Brightness).
-    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    ratio = 1.0 + 0.4 * (np.random.rand() - 0.5)
-    hsv[:,:,2] =  hsv[:,:,2] * ratio
-    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
